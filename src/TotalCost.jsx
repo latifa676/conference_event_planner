af6f2bf91304154d5a23fd077993dcd5374268f0
Latifa Mahdad <latifamahdad293@gmail.com>
2026-06-19 21:09:48 -0400

    Add Redux AV add-ons functionality

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -1,23 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
 import "./TotalCost.css";
 
 const TotalCost = ({ totalCosts, ItemsDisplay }) => {
-  
-
   return (
     <div className="pricing-app">
       <div className="display_box">
         <div className="header">
-          <p className="preheading"><h3>Total cost for the event</h3></p>
+          <h3>Total cost for the event</h3>
         </div>
+
+        <h2 id="pre_fee_cost_display" className="price">
+          ${totalCosts}
+        </h2>
+
         <div>
-          <h2 id="pre_fee_cost_display" className="price">
-           
-          </h2>
-         
-            <div>
-             
-            </div>
+          {ItemsDisplay && <ItemsDisplay />}
         </div>
       </div>
     </div>
